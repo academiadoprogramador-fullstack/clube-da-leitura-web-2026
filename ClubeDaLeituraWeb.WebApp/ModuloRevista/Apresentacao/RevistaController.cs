bb83b6d9d9e61b0f1ddb76f99f59f6d10928f092
Tiago Santini <tiagosantiniathayde@outlook.com>
2026-05-28 15:06:54 -0300

    Adiciona Cadastro, Edição e Exclusão de Revista

--- ClubeDaLeituraWeb.WebApp/ModuloRevista/Apresentacao/RevistaController.cs
+++ ClubeDaLeituraWeb.WebApp/ModuloRevista/Apresentacao/RevistaController.cs
@@ -35,4 +35,131 @@ public class RevistaController : Controller
 
         return View(listarVms);
     }
+
+    [HttpGet]
+    public ActionResult Cadastrar()
+    {
+        CadastrarRevistaViewModel cadastrarVm = new CadastrarRevistaViewModel(
+            string.Empty,
+            0,
+            DateTime.Now.Year,
+            string.Empty,
+            SelecionarCaixas()
+        );
+
+        return View(cadastrarVm);
+    }
+
+    [HttpPost]
+    public ActionResult Cadastrar(CadastrarRevistaViewModel cadastrarVm)
+    {
+        Caixa? caixaSelecionada = repositorioCaixa.SelecionarPorId(cadastrarVm.CaixaId);
+
+        if (caixaSelecionada == null)
+            ModelState.AddModelError(nameof(cadastrarVm.CaixaId), "Selecione uma caixa válida.");
+
+        if (cadastrarVm.AnoPublicacao > DateTime.Now.Year)
+            ModelState.AddModelError(nameof(cadastrarVm.AnoPublicacao), "O campo \"Ano de Publicação\" deve conter um ano válido.");
+
+        if (!ModelState.IsValid)
+            return View(cadastrarVm with { Caixas = SelecionarCaixas() });
+
+        Revista novaRevista = new Revista(
+            cadastrarVm.Titulo,
+            cadastrarVm.NumeroEdicao,
+            cadastrarVm.AnoPublicacao,
+            caixaSelecionada!
+        );
+
+        repositorioRevista.Cadastrar(novaRevista);
+
+        return RedirectToAction(nameof(Listar));
+    }
+
+    [HttpGet]
+    public ActionResult Editar(string id)
+    {
+        Revista? revista = repositorioRevista.SelecionarPorId(id);
+
+        if (revista == null)
+            return RedirectToAction(nameof(Listar));
+
+        EditarRevistaViewModel editarVm = new EditarRevistaViewModel(
+            id,
+            revista.Titulo,
+            revista.NumeroEdicao,
+            revista.AnoPublicacao,
+            revista.Caixa.Id,
+            SelecionarCaixas()
+        );
+
+        return View(editarVm);
+    }
+
+    [HttpPost]
+    public ActionResult Editar(EditarRevistaViewModel editarVm)
+    {
+        Revista? revista = repositorioRevista.SelecionarPorId(editarVm.Id);
+        Caixa? caixaSelecionada = repositorioCaixa.SelecionarPorId(editarVm.CaixaId);
+
+        if (revista == null)
+            return RedirectToAction(nameof(Listar));
+
+        if (caixaSelecionada == null)
+            ModelState.AddModelError(nameof(editarVm.CaixaId), "Selecione uma caixa válida.");
+
+        if (editarVm.AnoPublicacao > DateTime.Now.Year)
+            ModelState.AddModelError(nameof(editarVm.AnoPublicacao), "O campo \"Ano de Publicação\" deve conter um ano válido.");
+
+        if (!ModelState.IsValid)
+            return View(editarVm with { Caixas = SelecionarCaixas() });
+
+        Revista revistaAtualizada = new Revista(
+            editarVm.Titulo,
+            editarVm.NumeroEdicao,
+            editarVm.AnoPublicacao,
+            caixaSelecionada!,
+            revista.Status
+        );
+
+        repositorioRevista.Editar(editarVm.Id, revistaAtualizada);
+
+        return RedirectToAction(nameof(Listar));
+    }
+
+    [HttpGet]
+    public ActionResult Excluir(string id)
+    {
+        Revista? revista = repositorioRevista.SelecionarPorId(id);
+
+        if (revista == null)
+            return RedirectToAction(nameof(Listar));
+
+        ExcluirRevistaViewModel excluirVm = new ExcluirRevistaViewModel(
+            id,
+            revista.Titulo,
+            revista.NumeroEdicao,
+            revista.AnoPublicacao,
+            revista.Caixa.Etiqueta
+        );
+
+        return View(excluirVm);
+    }
+
+    [HttpPost]
+    public ActionResult Excluir(ExcluirRevistaViewModel excluirVm)
+    {
+        repositorioRevista.Excluir(excluirVm.Id);
+
+        return RedirectToAction(nameof(Listar));
+    }
+
+    private List<OpcaoCaixaViewModel> SelecionarCaixas()
+    {
+        List<Caixa> caixas = repositorioCaixa.SelecionarTodos();
+
+        return caixas
+            .Select(caixa => new OpcaoCaixaViewModel(caixa.Id, caixa.Etiqueta))
+            .ToList();
+    }
 }
